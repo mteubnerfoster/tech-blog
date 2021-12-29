a4cd8bf01a6bebd93af71bb1463712297fb4b16f
Megan Teubner-Foster <mteubnerfoster@gmail.com>
2021-12-28 20:45:13 -0800

    edited user route for functionality; seeing if it works

--- controllers/api/index.js
+++ controllers/api/index.js
@@ -4,8 +4,8 @@ const userRoutes = require('./user-routes.js');
 const postRoutes = require('./post-routes');
 const commentRoutes = require('./comment-routes');
 
-router.use('/user', userRoutes);
-router.use('/post', postRoutes);
-router.use('/comment', commentRoutes);
+router.use('/User', userRoutes);
+router.use('/Post', postRoutes);
+router.use('/Comment', commentRoutes);
 
 module.exports = router;
